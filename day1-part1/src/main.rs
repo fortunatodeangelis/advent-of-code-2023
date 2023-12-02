501cf7c83b28c1b3e9ac71d10514e7ba186cbc4e
Fortunato De Angelis <deangelis.fortunato@gmail.com>
2023-12-02 12:49:05 +0100

    fix: removes redundant code

--- day1-part1/src/main.rs
+++ day1-part1/src/main.rs
@@ -16,23 +16,16 @@ fn main() {
     while let Some(line) = lines.next() {
         // Extract the number from the line
         let string = line.chars().filter(|c| c.is_digit(10)).collect::<String>();
-        // if the string is length 1 multiply by 11
-        if string.len() == 1 {
-            // add the number to the sum
-            sum += string.parse::<i32>().unwrap() * 11;
-        } 
-        // if the string is length > 1
-        else if string.len() > 1 {
-            // Get First and last number of the string
-            let (first, last) = (string.chars().next().unwrap(), string.chars().last().unwrap());
-            format!("{}{}", first, last).parse::<i32>().unwrap();
 
-            // Merge of the two numbers for example first = 1 and last = 5, merge = 15
-            let merge = format!("{}{}", first, last).parse::<i32>().unwrap();
+        // Get First and last number of the string
+        let (first, last) = (string.chars().next().unwrap(), string.chars().last().unwrap());
+        format!("{}{}", first, last).parse::<i32>().unwrap();
 
-            // add the merge to the sum
-            sum += merge;
-        }
+        // Merge of the two numbers for example first = 1 and last = 5, merge = 15
+        let merge = format!("{}{}", first, last).parse::<i32>().unwrap();
+
+        // add the merge to the sum
+        sum += merge;
     }
     // Print the sum
     println!("Sum: {}", sum);
